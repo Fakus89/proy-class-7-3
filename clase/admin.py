cfb9e166ccdd18645e87210980a495ec984ae7de
Facundo Nieves <facundo.nieves@hotmail.com>
2022-03-12 16:41:39 -0300

    clase 20

--- clase/admin.py
+++ clase/admin.py
@@ -1,3 +1,10 @@
+from re import S
+from sqlite3 import Cursor
 from django.contrib import admin
-
+from .models import Curso,Profesor,Entregable,Estudiante
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
+admin.site.register(Entregable)
--- indice/urls.py
+++ indice/urls.py
@@ -3,10 +3,10 @@ from .views import inicio,otra_vista,numero_random,numero_usuario,anio_edad,mi_p
 
 urlpatterns = [
     
-    path("",inicio),
-    path("otra_vista/",otra_vista),
+    path("",inicio,name="inicio"),
+    path("otra_vista/",otra_vista,name="otra vista"),
     path("edad/<numero>",anio_edad),
-    path("mi_plantilla/",mi_plantilla),
+    path("mi_plantilla/",mi_plantilla,name="mi_plantilla"),
     path("dame-numero/<int:numero>",numero_usuario),
-    path("numero_random/",numero_random),
+    path("numero_random/",numero_random,name="numero_random"),
 ]
